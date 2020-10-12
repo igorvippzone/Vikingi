1c8002cdaaf862ee6c156859f64a95c0c24ea835
Игорь Агаларов <igorvippzone@mail.ru>
2020-10-12 09:13:33 +0300

    seasons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 $(function () {
-  $('[data-fancybox]').fancybox({
-    youtube: {
-      controls: 0,
-      showinfo: 0
+  // $('[data-fancybox]').fancybox({
+  //   youtube: {
+  //     controls: 0,
+  //     showinfo: 0
+  //   }
+  // });
+  $('.card-deck a').fancybox({
+    caption : function( instance, item ) {
+      return $(this).parent().find('.card-text').html();
     }
   });
 });
