59ba963c43458fde2b3176f718d20fd3802acfca
Игорь Агаларов <igorvippzone@mail.ru>
2020-10-12 10:23:41 +0300

    heroes1

--- js/main.js
+++ js/main.js
@@ -6,8 +6,24 @@ $(function () {
   //   }
   // });
   $('.card-deck a').fancybox({
-    caption : function( instance, item ) {
+    caption: function (instance, item) {
       return $(this).parent().find('.card-text').html();
     }
+  });
+
+
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    prevArrow: '<button class="slick-btn slick-prev"><img src="img/arrow-left.png" alt=""></button>',
+    prevArrow: '<button class="slick-btn slick-next"><img src="img/arrow-right.png" alt=""></button>',
+  });
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '.heroes__slider-img'
+
   });
 });
