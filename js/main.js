918e665ab94de09f32a2e8a5e8c9104fdc00aefc
Игорь Агаларов <igorvippzone@mail.ru>
2020-10-13 10:27:52 +0300

    page-1

--- js/main.js
+++ js/main.js
@@ -17,13 +17,13 @@ $(function () {
     slidesToScroll: 1,
     asNavFor: '.heroes__slider-text',
     prevArrow: '<button class="slick-btn slick-prev"><img src="img/arrow-left.png" alt=""></button>',
-    prevArrow: '<button class="slick-btn slick-next"><img src="img/arrow-right.png" alt=""></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="img/arrow-right.png" alt=""></button>',
   });
   $('.heroes__slider-text').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     fade: true,
-    asNavFor: '.heroes__slider-img'
-
+    asNavFor: '.heroes__slider-img',
+arrows: false,
   });
 });
